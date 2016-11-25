15fa2f3533bae22b626cdcc4630db23606f04151
grigore.stancul <grigore.stancul@become.eu>
2016-11-25 16:56:07 +0100

    First version of books portal

---
+++ books/src/main/java/com/grisha/books/entity/Review.java
@@ -0,0 +1,48 @@
+package com.grisha.books.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Review {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name="book_id")
+	private Book book;
+
+	@ManyToOne
+	private User user;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
