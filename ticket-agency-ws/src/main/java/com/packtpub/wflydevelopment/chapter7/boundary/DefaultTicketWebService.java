e2de0453257b9c5d050089f1ef7575b98bce592b
grigore.stancul <grigore.stancul@become.eu>
2016-12-09 16:43:39 +0100

    Add base classes

---
+++ ticket-agency-ws/src/main/java/com/packtpub/wflydevelopment/chapter7/boundary/DefaultTicketWebService.java
@@ -0,0 +1,33 @@
+package com.packtpub.wflydevelopment.chapter7.boundary;
+
+
+import javax.inject.Inject;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import java.io.Serializable;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@WebService(targetNamespace = "http://www.packtpub.com/", serviceName = "TicketWebService")
+public class DefaultTicketWebService implements TicketWebService, Serializable {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Inject
+    private TheatreBox theatreBox;
+
+    @WebMethod
+    @WebResult(name = "listSeats")
+    public List<SeatDto> getSeats() {
+        return theatreBox.getSeats().stream().map(SeatDto::fromSeat).collect(Collectors.toList());
+    }
+
+    @WebMethod
+    public void bookSeat(@WebParam(name = "seatId") int seatId) {
+        theatreBox.buyTicket(seatId);
+    }
+}
